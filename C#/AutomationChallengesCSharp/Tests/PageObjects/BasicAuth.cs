500147c726d42dceda7c64e5e5b243467cb76ca5
mkmnj <maik.manjura@tu-dortmund.de>
2020-03-09 21:29:54 +0100

    Added test for basic auth

---
+++ C#/AutomationChallengesCSharp/Tests/PageObjects/BasicAuth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace Tests.PageObjects
+{
+    class BasicAuth
+    {
+        private IWebDriver driver;
+        private IWebElement CongratulationsText => driver.FindElement(By.CssSelector("p"));
+
+        public BasicAuth(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public bool TextShouldBeVisibleAfterBasicAuth()
+        {
+            return CongratulationsText.Displayed;
+        }
+    }
+}
---
+++ C#/AutomationChallengesCSharp/Tests/TestSuiteBasicAuth.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Tests.PageObjects;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestSuiteBasicAuth : ITestTemplate
+    {
+        public IWebDriver driver;
+        public static string user = "admin";
+        public static string password = "admin";
+        public string credentials = user + ":" + password;
+
+        public string mainURL = "the-internet.herokuapp.com";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+
+            driver.Url = "https://" + credentials + "@" + mainURL + "/basic_auth";
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [TestMethod]
+        public void AddNoElements()
+        {
+            BasicAuth page = new BasicAuth(driver);
+
+            Assert.IsTrue(page.TextShouldBeVisibleAfterBasicAuth());
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+    }
+}
